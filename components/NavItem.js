a15964019dc7668cad9fe1e031bae4b97d7e0ef4
elcharitas <jonathanirhodia@gmail.com>
2022-04-18 12:22:40 +0100

    chore: add link support to nav items

--- components/NavItem.js
+++ components/NavItem.js
@@ -1,25 +1,27 @@
-import React from "react";
 import { Flex, Icon } from "@chakra-ui/react";
+import { Link } from "@saas-ui/react";
 
-export default function NavItem({ icon, href, children, ...rest }) {
+export default function NavItem({ icon, href = "#", children, ...rest }) {
     return (
-        <Flex
-            align="center"
-            px="4"
-            pl="4"
-            py="3"
-            cursor="pointer"
-            color={"white"}
-            _hover={{
-                bg: "tete.hover",
-                fontWeight: "bold",
-            }}
-            role="group"
-            transition=".15s ease"
-            {...rest}
-        >
-            {icon && <Icon mx="2" boxSize="4" as={icon} />}
-            {children}
-        </Flex>
+        <Link href={href}>
+            <Flex
+                align="center"
+                px="4"
+                pl="4"
+                py="3"
+                cursor="pointer"
+                color={"white"}
+                _hover={{
+                    bg: "tete.hover",
+                    fontWeight: "bold",
+                }}
+                role="group"
+                transition=".15s ease"
+                {...rest}
+            >
+                {icon && <Icon mx="2" boxSize="4" as={icon} />}
+                {children}
+            </Flex>
+        </Link>
     );
 }
