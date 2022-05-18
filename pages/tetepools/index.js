8374b6278bd25cba72c11de6ec00a3eb2985f73b
orglobal <orglobalng@gmail.com>
2022-05-18 14:52:49 +0100

    Add: Tete Pool Component

---
+++ pages/tetepools/index.js
@@ -0,0 +1,24 @@
+import { Flex, Text } from "@chakra-ui/react";
+import Page from "../../components/Page";
+import TetePools from "../../components/TetePools";
+
+export default function index({ sidebar }) {
+  return (
+    <>
+      <Page title="Tete Pools" sidebar={sidebar}>
+        <Flex direction="column" justifyContent="space-between" color="white">
+          <Flex
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Text my="2" id="live" fontSize="2xl" fontWeight="bold">
+              Tete Pools
+            </Text>
+          </Flex>
+        </Flex>
+        <TetePools />
+      </Page>
+    </>
+  );
+}
