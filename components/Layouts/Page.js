a32676a5f93c91360bf76b61e5bbc8ea63d54c0e
elcharitas <jonathanirhodia@gmail.com>
2022-05-20 12:18:16 +0100

    chore: extract slides into constants

--- components/Layouts/Page.js
+++ components/Layouts/Page.js
@@ -2,57 +2,14 @@ import { Box, Flex, Text } from "@chakra-ui/react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-import SlideCard from "../Cards/Slide";
 import Header from "./Header";
 import Footer from "./Footer";
 import Widget from "../Widgets/Base";
+import SlideCard from "../Cards/Slide";
 import SportWidget from "../Widgets/Sport";
+import { home_slides } from "../../utils/const";
 
 export default function Page({ sidebar, title, children }) {
-    const slides = [
-        {
-            bg: "tete.hover",
-            intro:
-                "Welcome to Tété, we are using Betting as an Instrument of Social Change.",
-            image: "/header.png",
-            buttons: [
-                {
-                    text: "Create Pool",
-                    href: "/pools/create",
-                },
-                {
-                    text: "Place a Bet",
-                },
-            ],
-        },
-        {
-            bg: "tete.card",
-            intro:
-                "Everytime you WIN, Someone on the streets also WIN. Together , let’s take Young Girls and Boys off the street.",
-            image: "/wallet.png",
-            buttons: [
-                {
-                    text: "Donate Now",
-                    bg: "white",
-                    color: "purple.500",
-                },
-                {
-                    text: "Learn More",
-                },
-            ],
-        },
-        {
-            bg: "tete.hover",
-            intro:
-                "From Grammy, BET Awards, Oscars, You can now stake on your Favourite nominee from any Category.",
-            image: "/grammy.png",
-            buttons: [
-                {
-                    text: "Start Here",
-                },
-            ],
-        },
-    ];
     return (
         <>
             <Header title={title} sidebar={sidebar} />
@@ -63,7 +20,7 @@ export default function Page({ sidebar, title, children }) {
             >
                 <Box w={{ base: "full", md: "77%" }} p="4">
                     <Carousel showThumbs={false} autoPlay infiniteLoop>
-                        {slides.map((slide, index) => (
+                        {home_slides.map((slide, index) => (
                             <SlideCard slide={slide} key={index} />
                         ))}
                     </Carousel>
