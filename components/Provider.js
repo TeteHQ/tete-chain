03a44d89acbe322b6f7cf5b6fc416d4171d3f180
elcharitas <jonathanirhodia@gmail.com>
2023-02-04 09:07:17 +0100

    feat: persist ud login

--- components/Provider.js
+++ components/Provider.js
@@ -6,44 +6,45 @@ import Head from "next/head";
 // 	// account: localStorage.getItem("account"),
 // });
 const DEFAULT_AUTH = {
-	get account() {
-		return JSON.parse(localStorage.getItem("account"));
-	},
-}
+    get account() {
+        if (typeof window === "undefined") return null;
+        return JSON.parse(localStorage.getItem("account"));
+    },
+};
 export const Context = createContext(DEFAULT_AUTH);
 
 export default function Provider({ children, value }) {
-	const [account, updateAccount] = useState(DEFAULT_AUTH.account?.[0]?.name || DEFAULT_AUTH.account?.[0]?.address || "");
-	const [balance, setBalance] = useState(0);
-	const [ethProvider, setProvider] = useState(null);
-	const [network, setNetwork] = useState(null);
-	const [title, setTitle] = useState("");
+    const [account, updateAccount] = useState([DEFAULT_AUTH.account?.[0]]);
+    const [balance, setBalance] = useState(0);
+    const [ethProvider, setProvider] = useState(null);
+    const [network, setNetwork] = useState(null);
+    const [title, setTitle] = useState("");
 
-	const setAccount = (account) => {
-		updateAccount(account);
-		localStorage.setItem("account", JSON.stringify(account));
-	};
+    const setAccount = (account) => {
+        updateAccount(account);
+        localStorage.setItem("account", JSON.stringify(account));
+    };
 
-	return (
-		<Context.Provider
-			value={{
-				account,
-				setAccount,
-				balance,
-				setBalance,
-				ethProvider,
-				setProvider,
-				network,
-				setNetwork,
-				title,
-				setTitle,
-				...value,
-			}}
-		>
-			<Head>
-				<title>{title} | Tete</title>
-			</Head>
-			{children}
-		</Context.Provider>
-	);
+    return (
+        <Context.Provider
+            value={{
+                account,
+                setAccount,
+                balance,
+                setBalance,
+                ethProvider,
+                setProvider,
+                network,
+                setNetwork,
+                title,
+                setTitle,
+                ...value,
+            }}
+        >
+            <Head>
+                <title>{title} | Tete</title>
+            </Head>
+            {children}
+        </Context.Provider>
+    );
 }
--- utils/hooks.js
+++ utils/hooks.js
@@ -5,113 +5,112 @@ import { Context } from "../components/Provider";
 import { provider } from "./ethers";
 
 const uauth = new UAuth({
-	clientID: "956e6ba4-d8d4-48c9-b64f-af4d258d3453",
-	redirectUri: "https://tete-chain.com",
-	scope: "openid wallet",
+    clientID: "956e6ba4-d8d4-48c9-b64f-af4d258d3453",
+    redirectUri: "http://localhost:3000",
+    scope: "openid wallet",
 });
 
 export function useWallet({ sync = false, auto = true } = {}) {
-	const {
-		account,
-		setAccount,
-		balance,
-		setBalance,
-		ethProvider,
-		setProvider,
-		network,
-	} = useContext(Context);
+    const {
+        account,
+        setAccount,
+        balance,
+        setBalance,
+        ethProvider,
+        setProvider,
+        network,
+    } = useContext(Context);
 
-	const snackbar = useSnackbar();
+    const snackbar = useSnackbar();
 
-	const connected = useMemo(
-		() => (account?.length > 0 ? account[0] : null),
-		[account]
-	);
+    const connected = useMemo(() => (account?.length > 0 ? account[0] : null), [
+        account,
+    ]);
 
-	const connectWallet = useCallback(() => {
-		if (sync) {
-			setProvider(provider.ethersSync(network));
-		} else
-			provider
-				.ethers(network)
-				.catch(() => snackbar.error("Failed to connect to the network"))
-				.then(setProvider);
-	}, [sync, setProvider, network, snackbar]);
+    const connectWallet = useCallback(() => {
+        if (sync) {
+            setProvider(provider.ethersSync(network));
+        } else
+            provider
+                .ethers(network)
+                .catch(() => snackbar.error("Failed to connect to the network"))
+                .then(setProvider);
+    }, [sync, setProvider, network, snackbar]);
 
-	const connectUDWallet = async () => {
-		try {
-			const authorization = await uauth.loginWithPopup();
-			const address = authorization.idToken.wallet_address;
-			setAccount([{ address, name: authorization.idToken.sub }]);
-		} catch (error) {
-			console.error(error);
-		}
-	};
+    const connectUDWallet = async () => {
+        try {
+            const authorization = await uauth.loginWithPopup();
+            const address = authorization.idToken.wallet_address;
+            setAccount([{ address, name: authorization.idToken.sub }]);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-	const disconnectUDWallet = useCallback(async () => {
-		await uauth.logout();
-		setAccount([]);
-		snackbar.success("Wallet has been disconnected");
+    const disconnectUDWallet = useCallback(async () => {
+        await uauth.logout();
+        setAccount([]);
+        snackbar.success("Wallet has been disconnected");
 
-		console.log("Logged out with Unstoppable");
-	}, [snackbar, setAccount]);
+        console.log("Logged out with Unstoppable");
+    }, [snackbar, setAccount]);
 
-	const disconnectWallet = useCallback(() => {
-		if (typeof connected === "object") {
-			return disconnectUDWallet();
-		}
-		setAccount([]);
-		setBalance(0);
-		provider.clearCachedProvider();
-		setProvider(null);
-		snackbar.error("Disconnected from wallet");
-	}, [
-		connected,
-		disconnectUDWallet,
-		setAccount,
-		setBalance,
-		setProvider,
-		snackbar,
-	]);
-	const syncProvider = useCallback(() => {
-		setProvider(provider.ethersSync(network));
-	}, [network, setProvider]);
+    const disconnectWallet = useCallback(() => {
+        if (typeof connected === "object") {
+            return disconnectUDWallet();
+        }
+        setAccount([]);
+        setBalance(0);
+        provider.clearCachedProvider();
+        setProvider(null);
+        snackbar.error("Disconnected from wallet");
+    }, [
+        connected,
+        disconnectUDWallet,
+        setAccount,
+        setBalance,
+        setProvider,
+        snackbar,
+    ]);
+    const syncProvider = useCallback(() => {
+        setProvider(provider.ethersSync(network));
+    }, [network, setProvider]);
 
-	useEffect(() => {
-		if (auto) connectWallet();
-	}, [auto, connectWallet]);
+    useEffect(() => {
+        if (auto) connectWallet();
+    }, [auto, connectWallet]);
 
-	useEffect(() => {
-		ethProvider
-			?.listAccounts()
-			.then(setAccount)
-			.then(() => snackbar("Wallet connected successfully!"));
-	}, [ethProvider, ethProvider?.listAccounts, setAccount, snackbar]);
+    useEffect(() => {
+        ethProvider
+            ?.listAccounts()
+            .then(setAccount)
+            .then(() => snackbar("Wallet connected successfully!"));
+    }, [ethProvider, ethProvider?.listAccounts, setAccount, snackbar]);
 
-	useEffect(() => {
-		if (connected) ethProvider?.getBalance(connected).then(setBalance);
-	}, [account, connected, setBalance, ethProvider]);
+    useEffect(() => {
+        if (connected) ethProvider?.getBalance(connected).then(setBalance);
+    }, [account, connected, setBalance, ethProvider]);
 
-	return {
-		balance,
-		account,
-		connected,
-		provider: ethProvider,
-		get isConnected() {
-			return account?.length > 0;
-		},
-		get address() {
-			if (this.connected && typeof this.connected === "object") {
-				return this.connected?.name;
-			}
-			return this.connected
-				? this.connected.slice(0, 6) + "..." + this.connected.slice(-4)
-				: null;
-		},
-		connectWallet,
-		connectUDWallet,
-		disconnectWallet,
-		disconnectUDWallet,
-		syncProvider,
-	};
+    return {
+        balance,
+        account,
+        connected,
+        provider: ethProvider,
+        get isConnected() {
+            return account?.length > 0;
+        },
+        get address() {
+            if (this.connected && typeof this.connected === "object") {
+                return this.connected?.name;
+            }
+            return this.connected
+                ? this.connected.slice(0, 6) + "..." + this.connected.slice(-4)
+                : null;
+        },
+        connectWallet,
+        connectUDWallet,
+        disconnectWallet,
+        disconnectUDWallet,
+        syncProvider,
+    };
 }
